68a417af3dec7a985fde246dc6f93b688cf243d4
cjohnsonal <christopher.johnson@medicaid.alabama.gov>
2016-03-22 14:42:12 -0500

    Meaningless fix to update author

--- CCD Validator/Program.cs
+++ CCD Validator/Program.cs
@@ -8,9 +8,6 @@ namespace CCD_Validator
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
